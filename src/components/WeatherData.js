294ebbbca3955b86f5f0b8525d97965db986d0c9
redsdavid <redsdavid93@gmail.com>
2019-01-09 23:16:50 -0500

    Add weather constants
    Add weather constants

--- src/components/WeatherData.js
+++ src/components/WeatherData.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import WeatherTemperature from './WeatherTemperature'
 import WeatherExtraInfo from './WeatherExtraInfo'
+import {
+    FOG,
+    CLOUD,
+    CLOUDY,
+    SUN,
+    RAIN,
+    SNOW,
+    WINDY,
+} from './../constants/weathers';
 
 const WeatherData = () => (
     <div>
         <WeatherTemperature 
             temperature={20} 
-            weatherState={'sun'}>
+            weatherState={SUN}>
         </WeatherTemperature>
         <WeatherExtraInfo humidity={80} wind="10 m/s"></WeatherExtraInfo>
     </div>
