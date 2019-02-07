53dfb7dc6d85200481760efe46bff8a6ef4ee0a0
redsdavid <redsdavid93@gmail.com>
2019-02-06 21:52:42 -0500

    Add States to WeatherLocation Component and Button events

--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -3,7 +3,7 @@
  import WeatherData from './WeatherData'
  import './styles.css';
  import {
-    SUN
+    SUN,WINDY
 } from './.././.././constants/weathers';
 
  const data={
@@ -13,12 +13,36 @@
      wind: '50 m/s'
  }
 
+ const data2={
+    temperature: 45,
+    weatherState: WINDY,
+    humidity: 50,
+    wind: '10 m/s'
+}
+
  class WeatherLocation extends Component {
+
+    constructor(){
+        super();
+        this.state={
+            city:"GONORREA!",
+            data: data
+        }
+    }
+
+    handleUpdateClick = () => {
+        this.setState({
+            city: "UY JUEPUTA!",
+            data:data2
+        })
+    }
      render(){
+         const {city , data } = this.state
          return (
         <div className="weatherLocationCont">
-             <Location city={'TaBogo!'}></Location>
+             <Location city={city}></Location>
              <WeatherData data={data}></WeatherData>
+             <button onClick={this.handleUpdateClick}>Actualizar! <span role="img" aria-label="doggie">🐶</span></button>
         </div>
          )
      }
