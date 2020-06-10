cdb4df46dc8c877eacd4610cf98b36fc27a4f273
redsdavid <redsdavid93@gmail.com>
2020-06-09 22:35:14 -0500

    Refactor: Split  App.js to container, add city reducer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
 import Paper from '@material-ui/core/Paper';
 import AppBar from '@material-ui/core/AppBar';
 import Typography from '@material-ui/core/Typography';
 import Toolbar from '@material-ui/core/Toolbar';
 import { Grid, Col, Row } from 'react-flexbox-grid';
-import LocationList from './components/LocationList';
+import LocationListContainer from './containers/LocationListContainer';
 import ForecastExtended from './components/ForecastExtendend';
-import { setCity } from './actions'
 import './App.css';
 
 const cities = [
@@ -27,13 +25,6 @@ class App extends Component {
     };
   }
 
-  handleSelectedLocation = city => {
-    this.setState({ city });
-    console.log(`handleSelectedLocation ${city}`);
-    
-    this.props.setCity(city);
-  }
-
   render() {
     const { city } = this.state;
     return (
@@ -49,10 +40,8 @@ class App extends Component {
         </Row>
         <Row>
           <Col xs={12} md={6}>
-            <LocationList cities = {cities}
-            onSelectedLocation={this.handleSelectedLocation}></LocationList>
+            <LocationListContainer cities={cities}></LocationListContainer>
           </Col>
-
           <Col xs={12} md={6} className="extended-weather-cont">
             <Paper>
               <div className="details">
@@ -68,9 +57,4 @@ class App extends Component {
   }
 }
 
-
-const mapDispatchToPropsActions = dispatch =>({
-  setCity: value =>  dispatch(setCity(value))
-});
-const AppConnected = connect(null, mapDispatchToPropsActions)(App)
-export default AppConnected;
+export default App;
